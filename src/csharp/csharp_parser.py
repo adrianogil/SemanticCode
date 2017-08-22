5138a289bfe3876991ac0340361883dd4a8b5204
Adriano Gil <adrianogil.san@gmail.com>
2017-08-21 20:18:29 -0400

    WIP Basic code to test csharp parser

---
+++ src/csharp/csharp_parser.py
@@ -0,0 +1,7 @@
+
+sys.path.append('framework')
+import token_parser
+
+class BasicCSharpParser(token_parser.TokenParser):
+    def __init(self, parse_behavior = None):
+        super(BasicCSharpParser, self).__init__(self, parse_behavior)
---
+++ src/csharp/test/test_csharp_utils.py
@@ -0,0 +1,5 @@
+
+
+class CSharpTest():
+    def __init__(self):
+            
